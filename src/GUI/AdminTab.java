92a807058520f7df6144d069e83e2a30d356e14c
Rasmus <rasmus_soe@hotmail.com>
2022-11-30 13:01:19 +0100

    Bøh

--- src/GUI/AdminTab.java
+++ src/GUI/AdminTab.java
@@ -1,12 +1,17 @@
 package GUI;
 
 import Application.Controller.Controller;
+import Application.Model.Conference;
+import Application.Model.Participant;
 import javafx.geometry.Insets;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class AdminTab extends GridPane {
     private TextArea conferenceDeltagere;
@@ -24,6 +29,11 @@ public class AdminTab extends GridPane {
         adminPane.setHgap(10);
         adminPane.setVgap(10);
         adminPane.setStyle("-fx-border-color: black");
+
+
+        ListView<ArrayList<Conference>> lvwConf = new ListView<ArrayList<Conference>>();
+        adminPane.add(lvwConf,0,0);
+        lvwConf.getItems().add(Controller.getConferences());
 
         conferenceBox = new ComboBox<>();
         conferenceBox.getItems().add(Controller.getConferences());
